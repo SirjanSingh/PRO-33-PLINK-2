7a87d3cdfe7181f4995ad5c3f8fc7045f90eaab8
Sirjan Singh <sirjan.singh036@gmail.com>
2024-10-31 18:42:53 +0700

    tried moving the particles to the bottom of the box

--- Ground.js
+++ Ground.js
@@ -4,11 +4,11 @@ class Ground{
         var option={
             isStatic: true
         }
-this.body= Bodies.rectangle(x,y,width,height,option);
-this.width=width;
-this.height=height;
-World.add (world,this.body);
-}
+        this.body= Bodies.rectangle(x,y,width,height,option);
+        this.width=width;
+        this.height=height;
+        World.add (world,this.body);
+    }
 
     display(){
         var pos=this.body.position;
